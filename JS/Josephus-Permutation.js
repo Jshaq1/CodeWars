937a03267a146af4bd7c000127a97abe2366a0d3
Jordan <Jordanshaqallen@gmail.com>
2023-09-05 09:11:40 +0800

    JS kata ext

--- JS/Josephus-Permutation.js
+++ JS/Josephus-Permutation.js
@@ -1,10 +1,14 @@
 function josephus(items, k) {
-    var result = []
+    var itemsArr = [...Array(items).keys()].map(x => x + 1)
     var index = 0
-    while (items.length > 0) {
-        index = (index + k - 1) % items.length;
-        result = result.concat(items.splice(index, 1));
-
+    while (itemsArr.length > 1) {
+        console.log((index + k - 1) % itemsArr.length)
+        index = (index + k - 1) % itemsArr.length;
+        itemsArr.splice(index, 1);
     }
-    return result;
+    return itemsArr[0];
 }
+
+
+// console.log(josephus(7, 3))
+console.log(6 % 5)
